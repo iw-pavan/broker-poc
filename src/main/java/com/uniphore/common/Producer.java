995a1d462e706ecb2432ff6ed72a09f8e4d830ab
shubhamInfoWorks <47854756+shubhamInfoWorks@users.noreply.github.com>
2025-08-25 18:23:45 +0530

    fit kafka to queue system

--- src/main/java/com/uniphore/common/Producer.java
+++ src/main/java/com/uniphore/common/Producer.java
@@ -3,7 +3,6 @@ package com.uniphore.common;
 import java.io.Closeable;
 
 public interface Producer extends Closeable {
-
     void produce(Queue queue, byte[] message, MessagePriority priority);
 
 }
--- src/main/java/com/uniphore/main/Runner.java
+++ src/main/java/com/uniphore/main/Runner.java
@@ -1,13 +1,14 @@
 package com.uniphore.main;
 
 import com.uniphore.common.BrokerFactory;
+import com.uniphore.kafka.KafkaBrokerFactory;
 import com.uniphore.rabbitmq.RabbitMQBrokerFactory;
 
 public class Runner {
 
     protected static final int THREADS = 4;
     protected static final int MESSAGES_PER_THREAD = 1000;
-    private static final BrokerFactory BROKER_FACTORY = new RabbitMQBrokerFactory();
+    private static final BrokerFactory BROKER_FACTORY = new KafkaBrokerFactory();
 
     protected static BrokerFactory getBrokerFactory() {
         return BROKER_FACTORY;
